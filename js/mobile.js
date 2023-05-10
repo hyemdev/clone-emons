60107d5e677b2d4d499ed9cc721502bd1a114234
hyemdev <devdev.hyem@gmail.com>
2023-05-10 18:13:06 +0900

    feat : 1차 최종merge용

--- js/mobile.js
+++ js/mobile.js
@@ -3,15 +3,23 @@ window.onload = function () {
     // 모바일 버튼
     let mobileDropdown = document.querySelector(".mobile-dropdown");
     let mobileCate = document.querySelector(".mobile-cate");
-    let mobileDropdownClose = document.querySelector(".mobile-dropdown-close");
     
     mobileCate.addEventListener("click", function () {
-      let hasActive =  mobileDropdown.classList.contains("mobile-dropdown-active");
-      console.log(hasActive);
-      if (hasActive === false) {
+        let hasActive =  mobileDropdown.classList.contains("mobile-dropdown-active");
+        console.log(hasActive);
+        if (hasActive === false) {
         mobileDropdown.classList.add("mobile-dropdown-active"); 
-      } else { //아니라면
+        } else { 
         mobileDropdown.classList.remove("mobile-dropdown-active");
-      }
+        }
     });
-}
+
+    // gotop 배너
+    let goTop = document.querySelector(".gotop");
+    goTop.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+};
