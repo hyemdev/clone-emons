a213cf5956c5f543694d902d75151f09cfc95ed8
hyemdev <devdev.hyem@gmail.com>
2023-05-10 16:07:11 +0900

    feat : 모바일 카테 js 수정

--- js/mobile.js
+++ js/mobile.js
@@ -3,13 +3,15 @@ window.onload = function () {
     // 모바일 버튼
     let mobileDropdown = document.querySelector(".mobile-dropdown");
     let mobileCate = document.querySelector(".mobile-cate");
+    let mobileDropdownClose = document.querySelector(".mobile-dropdown-close");
+    
     mobileCate.addEventListener("click", function () {
       let hasActive =  mobileDropdown.classList.contains("mobile-dropdown-active");
       console.log(hasActive);
       if (hasActive === false) {
-        mobileDropdown.classList.add("mobile-dropdown-active"); //add를 해라
+        mobileDropdown.classList.add("mobile-dropdown-active"); 
       } else { //아니라면
-        mobileDropdown.classList.remove("mobile-dropdown-active"); //remove를 해라
+        mobileDropdown.classList.remove("mobile-dropdown-active");
       }
     });
 }
